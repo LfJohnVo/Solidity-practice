6635f4866a3b2ebb7d87d04dfec94db91b9f9f83
John Vo <vojohn95@gmail.com>
2021-09-18 20:38:03 -0500

    Moved Owner to class

--- MessageStore.sol
+++ MessageStore.sol
@@ -1,29 +1,24 @@
 //remix ide
 pragma solidity ^0.4.24;
 
+import "./Ownable.sol";
+
+
 //Contract name
-contract MessageStore {
-    address private owner;
+contract MessageStore is Ownable {
+    
     string private message;
-
-    constructor() public {
-        owner = msg.sender;
-    }
-
+    
     //payable make necesary payment
-    function setMessage(string newMessage) public payable {
-        //obligatory 3 ether on every setMessage
+    function setMessage(string newMessage) public payable{
         require(msg.value == 3 ether);
         message = newMessage;
     }
-
+    
     function getMessage() public view returns (string) {
         return message;
     }
-
-    modifier isOwner() {
-        require(owner == msg.sender);
-        //mecanismo de placeholder
-        _;
-    }
+    
+    
+    
 }
