7ac9fc565e04bb0b632ccdbac1604615d4d8919c
John Vo <vojohn95@gmail.com>
2021-09-18 20:26:23 -0500

    Se añadio metodo de pago

--- MessageStore.sol
+++ MessageStore.sol
@@ -1,20 +1,29 @@
-//remix ide 
+//remix ide
 pragma solidity ^0.4.24;
+
 //Contract name
 contract MessageStore {
-    
     address private owner;
     string private message;
-    
+
     constructor() public {
         owner = msg.sender;
     }
-    
-    function setMessage(string newMessage) public {
+
+    //payable make necesary payment
+    function setMessage(string newMessage) public payable {
+        //obligatory 3 ether on every setMessage
+        require(msg.value == 3 ether);
         message = newMessage;
     }
-    
+
     function getMessage() public view returns (string) {
         return message;
+    }
+
+    modifier isOwner() {
+        require(owner == msg.sender);
+        //mecanismo de placeholder
+        _;
     }
 }
